84b604e9e68cfe87884612a86d799dc82423687c
did250 <gmldnjs2500@naver.com>
2022-07-03 03:40:52 +0900

    (recommit)

---
+++ meus/ViewModel.swift
@@ -0,0 +1,122 @@
+import Foundation
+import Combine
+import Firebase
+import FirebaseDatabase
+import CodableFirebase
+
+class ViewModel: ObservableObject {
+    
+    var ref : DatabaseReference = Database.database().reference()
+    
+    @Published var userinfo : userstruct = userstruct(Frequest: [""], Grequest: [""], friends: [""], groups: ["1"], id: "1", key: "", name: "0", schedules: [[""]], uid: "")
+    
+    init(){
+        print("VM init")
+    }
+}
+
+extension ViewModel {
+    func login( userEmail: String, userPassword: String, completion : @escaping (Int) -> ()){
+        return Auth.auth().signIn(withEmail: userEmail, password: userPassword) { [weak self] authResult, error in guard self != nil else { return }
+            if authResult != nil {
+                let flag = 1
+                completion(flag)
+            }
+            else {
+                let flag = 2
+                completion(flag)
+            }
+            
+        }
+    }
+    func Loaduser(completion: @escaping (userstruct) -> ()){
+        let uid = Auth.auth().currentUser?.uid
+        ref.child("users").child(uid ?? "anyvalue").getData {
+            (error, snapshot) in
+            if let error = error {
+                print("Error \(error)")
+            }
+            else {
+                let value = snapshot?.value
+                if let data = try? FirebaseDecoder().decode(userstruct.self, from: value){
+                    self.userinfo = data
+                    
+                    completion(data)
+                }
+                else {
+                    print("Error")
+                }
+            }
+        }
+    }
+    func AddGroup(new: String, completion: @escaping (Int)->()){
+        self.userinfo.groups.append(new)
+        self.ref.child("users").child(self.userinfo.uid).updateChildValues(["groups": self.userinfo.groups])
+        completion(1)
+    }
+    
+    func sendFrequest(friendid : String, flag : String, completion: @escaping (Int) -> ()) {
+        var frienduid : String = ""
+        self.ref.child("users").queryOrdered(byChild: "id").queryEqual(toValue: friendid).observeSingleEvent(of: .value, with: {
+            snapshot in
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                frienduid = snap.key
+                if flag == "GroupInvite" {
+                    self.ref.child("users").child(frienduid).child("Grequest").getData {
+                        (error,snapshot) in
+                        if let error = error {
+                            print("Error \(error)")
+                        }
+                        else{
+                            guard let value = snapshot?.value else {return}
+                            print(value)
+                            if let data2 = try? FirebaseDecoder().decode([String].self, from: value){
+                                var array: [String] = data2
+                                if (!array.contains(self.userinfo.id)){
+                                    array.append(self.userinfo.id)
+                                    self.ref.child("users").child(frienduid).updateChildValues(["Grequest": array])
+                                    completion(1)
+                                   
+                                }
+                                else {
+                                    completion(2)
+                                }
+                            }
+                            else{
+                                print("Error")
+                            }
+                        }
+                    }
+                }
+                else{
+                    print("dsflj;asfjds;lfjsald")
+                    print(frienduid)
+                    self.ref.child("users").child(frienduid).child("Frequest").getData {
+                        (error,snapshot) in
+                        if let error = error {
+                            print("Error \(error)")
+                        }
+                        else{
+                            guard let value = snapshot?.value else {return}
+                            if let data2 = try? FirebaseDecoder().decode([String].self, from: value){
+                                var array: [String] = data2
+                                if (!array.contains(self.userinfo.id)){
+                                    array.append(self.userinfo.id)
+                                    self.ref.child("users").child(frienduid).updateChildValues(["Frequest": array])
+                                    completion(3)
+                                }
+                                else {
+                                    completion(4)
+                                }
+                            }
+                            else{
+                                print("Error")
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    }
+}
